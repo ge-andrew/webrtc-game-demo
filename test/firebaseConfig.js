40977f7e486d01adbc7e6c9f44fe64b48abb2941
jams <36876904+jamsge@users.noreply.github.com>
2024-01-05 20:59:31 -0800

    firebase config in its own file

---
+++ test/firebaseConfig.js
@@ -0,0 +1,13 @@
+// YOUR FIREBASE CONFIG GOES HERE
+// REMEMBER TO KEEP THE export KEYWORD
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAThirM3igPr7FDi0Y-mt7Fw1L2oITZhm8",
+    authDomain: "webrtc-game-test-1.firebaseapp.com",
+    databaseURL: "https://webrtc-game-test-1-default-rtdb.firebaseio.com",
+    projectId: "webrtc-game-test-1",
+    storageBucket: "webrtc-game-test-1.appspot.com",
+    messagingSenderId: "870730742692",
+    appId: "1:870730742692:web:87efba047ec36e94971f31",
+    measurementId: "G-KRX0W3PKYS"
+};
