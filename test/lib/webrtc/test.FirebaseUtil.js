ca793fd19db341e0c0e1d3e77ea2dd84322d4c19
jams <36876904+jamsge@users.noreply.github.com>
2024-01-10 18:53:32 -0800

    fix issue with test scripts not running

---
+++ test/lib/webrtc/test.FirebaseUtil.js
@@ -0,0 +1,15 @@
+import { initialization } from "../../../lib/webrtc/FirebaseUtil.js";
+
+it('should return -1 when the value is not present', function(){
+  chai.assert.equal(-1, [1,2,3].indexOf(4));
+});
+
+// describe('Array', function () {
+//     describe('#indexOf()', function () {
+//       it('should return -1 when the value is not present', function () {
+//         assert.equal([1, 2, 3].indexOf(4), -1);
+//       });
+//     });
+// });
+
+console.log("hi");
